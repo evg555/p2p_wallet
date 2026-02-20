663f34455541a7c3e3928753102d2a00b2cfb1d4
evg <ontropos@gmail.com>
2026-02-20 13:55:28 +0300

    added handlers

---
+++ cmd/app/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"p2p_wallet/internal/handler"
+)
+
+func main() {
+	handler := handler.New()
+}
---
+++ internal/handler/handler.go
@@ -0,0 +1,31 @@
+package handler
+
+import (
+	"net/http"
+
+	"p2p_wallet/internal/api"
+)
+
+type handler struct{}
+
+var _ api.ServerInterface = (*handler)(nil)
+
+func New() *handler {
+	return &handler{}
+}
+
+func (h *handler) LoginUser(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotImplemented)
+}
+
+func (h *handler) RegisterUser(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotImplemented)
+}
+
+func (h *handler) GetUserById(w http.ResponseWriter, r *http.Request, id api.UserId) {
+	w.WriteHeader(http.StatusNotImplemented)
+}
+
+func (h *handler) LogoutUser(w http.ResponseWriter, r *http.Request, id api.UserId) {
+	w.WriteHeader(http.StatusNotImplemented)
+}
