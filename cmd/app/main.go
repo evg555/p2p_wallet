7020511f44de30ed1bbcc97c26d54a526d928135
evg <ontropos@gmail.com>
2026-02-20 14:11:58 +0300

    added server

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,9 +1,51 @@
 package main
 
 import (
+	"context"
+	"errors"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"p2p_wallet/internal/api"
 	"p2p_wallet/internal/handler"
 )
 
+var srvAddress = "localhost:8080"
+
 func main() {
-	handler := handler.New()
+	h := handler.New()
+	router := api.Handler(h)
+
+	srv := &http.Server{
+		Addr:              srvAddress,
+		Handler:           router,
+		ReadHeaderTimeout: 5 * time.Second,
+	}
+
+	go func() {
+		log.Printf("http server started on :%s", srvAddress)
+		if err := srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			log.Fatalf("http server failed: %v", err)
+		}
+	}()
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM)
+	<-stop
+
+	log.Printf("http server is stopping...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Printf("graceful shutdown failed: %v", err)
+		_ = srv.Close()
+	}
+
+	log.Printf("http server stopped")
 }
